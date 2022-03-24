f4a0b94908783153545b4bdea0b023ba3c909086
mjn81 <mjjnn81@gmail.com>
2022-03-24 22:39:16 +0430

    working on rendering body

--- public/js/classBase.js
+++ public/js/classBase.js
@@ -3,8 +3,7 @@ const scoreBoard = document.querySelector("#score");
 const resetBtn = document.querySelector("#reset");
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-const TILE_COUNT = 30, TILE_SIZE = 25;
-let velocity = [0, 0], score = 0, tailLength = 0;
+const TILE_COUNT = 30, TILE_SIZE = 25, WIDTH = canvas.width, HEIGHT = canvas.height, SHADOW_SIZE = 20;
 var KEYS;
 (function (KEYS) {
     KEYS["UP"] = "ArrowUp";
@@ -13,10 +12,17 @@ var KEYS;
     KEYS["RIGHT"] = "ArrowRight";
     KEYS["SPACE"] = "Space";
 })(KEYS || (KEYS = {}));
+let velocity = [0, 0, null], deltaTime = 0, speed = 100, score = 0, tailLength = 0, isGameFinished = false;
 var COLORS;
 (function (COLORS) {
     COLORS["SHEAD"] = "#FFF";
-    COLORS["SBODY"] = "##E1E1E1";
+    COLORS["SHEAD_SHADOW"] = "rgba(255,255,255,0.3 )";
+    COLORS["SBODY"] = "#E1E1E1";
+    COLORS["TILE_BORDER"] = "#222738";
+    COLORS["TILE"] = "#181825";
+    COLORS["APPLE"] = "red";
+    // TODO: find mild shadow color
+    COLORS["APPLE_SHADOW"] = "red";
 })(COLORS || (COLORS = {}));
 class Tile {
     constructor(x, y, color) {
@@ -27,10 +33,22 @@ class Tile {
 }
 class Snake {
     constructor() {
+        this.changeSnakePositon = () => {
+            this.head.x += velocity[0];
+            this.head.y += velocity[1];
+        };
         this.drawSnake = () => {
+            ctx.fillStyle = COLORS.SBODY;
+            for (let part of this.body) {
+                ctx.fillRect(part.x * TILE_SIZE, part.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            }
+            this.body.push(this.head);
+            if (this.body.length > tailLength) {
+                this.body.shift();
+            }
             ctx.fillStyle = this.head.color;
-            ctx.shadowColor = "rgba(255,255,255,.3 )";
-            ctx.shadowBlur = 20;
+            ctx.shadowColor = COLORS.SHEAD_SHADOW;
+            ctx.shadowBlur = SHADOW_SIZE;
             ctx.fillRect(this.head.x * TILE_SIZE, this.head.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             ctx.shadowBlur = 0;
         };
@@ -38,17 +56,83 @@ class Snake {
         this.body = [];
     }
 }
+const snake = new Snake();
+class Apple {
+    constructor() {
+        // TODO: create random colors for apple
+        this.randomGenerator = () => {
+            const xPos = Math.floor(Math.random() * TILE_COUNT), yPos = Math.floor(Math.random() * TILE_COUNT);
+            return new Tile(xPos, yPos, COLORS.APPLE);
+        };
+        this.drawApple = () => {
+            ctx.shadowColor = COLORS.APPLE_SHADOW;
+            ctx.shadowBlur = SHADOW_SIZE;
+            ctx.fillStyle = this.position.color;
+            ctx.fillRect(this.position.x * TILE_SIZE, this.position.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            ctx.shadowBlur = 0;
+        };
+        this.checkAppleCollision = () => {
+            if (this.position.x == snake.head.x && this.position.y == snake.head.y) {
+                this.position = this.randomGenerator();
+                tailLength++;
+                score++;
+            }
+        };
+        this.position = this.randomGenerator();
+    }
+}
+const apple = new Apple();
 window.addEventListener("keydown", (e) => {
     switch (e.code) {
         case KEYS.DOWN:
+            if (velocity[2] == KEYS.UP) {
+                return;
+            }
+            velocity = [0, 1, KEYS.DOWN];
+            break;
+        case KEYS.UP:
+            if (velocity[2] == KEYS.DOWN) {
+                return;
+            }
+            velocity = [0, -1, KEYS.UP];
+            break;
+        case KEYS.RIGHT:
+            if (velocity[2] == KEYS.LEFT) {
+                return;
+            }
+            velocity = [1, 0, KEYS.RIGHT];
+            break;
+        case KEYS.LEFT:
+            if (velocity[2] == KEYS.RIGHT) {
+                return;
+            }
+            velocity = [-1, 0, KEYS.LEFT];
             break;
     }
 });
-const snake = new Snake();
 class Game {
     constructor() {
-        this.drawGame = (deltaTime) => {
-            snake.drawSnake();
+        this.drawTileMap = () => {
+            ctx.strokeStyle = COLORS.TILE_BORDER;
+            ctx.fillStyle = COLORS.TILE;
+            for (let i = 0; i < TILE_COUNT; i++) {
+                for (let j = 0; j < TILE_COUNT; j++) {
+                    ctx.fillRect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                    ctx.strokeRect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+            }
+        };
+        this.drawGame = (time) => {
+            if (time - deltaTime < speed) {
+            }
+            else {
+                snake.changeSnakePositon();
+                apple.checkAppleCollision();
+                this.drawTileMap();
+                apple.drawApple();
+                snake.drawSnake();
+                deltaTime = time;
+            }
             requestAnimationFrame(this.drawGame);
         };
     }
--- src/ts/classBase.ts
+++ src/ts/classBase.ts
@@ -1,11 +1,12 @@
 const scoreBoard = document.querySelector("#score");
 const resetBtn = document.querySelector("#reset");
 const canvas = <HTMLCanvasElement>document.querySelector("#canvas");
-const ctx = <CanvasRenderingContext2D> canvas.getContext("2d");
-const TILE_COUNT: number = 30, TILE_SIZE: number = 25;
-let velocity: [number, number] = [0, 0],
-	score: number = 0,
-	tailLength: number = 0;
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+const TILE_COUNT: number = 30,
+	TILE_SIZE: number = 25,
+	WIDTH: number = canvas.width,
+	HEIGHT: number = canvas.height,
+	SHADOW_SIZE: number = 20;
 
 enum KEYS {
 	UP = "ArrowUp",
@@ -15,9 +16,23 @@ enum KEYS {
 	SPACE = "Space",
 }
 
+let velocity: [number, number , KEYS] = [0, 0 , null],
+	deltaTime: number = 0,
+	speed: number = 100,
+	score: number = 0,
+	tailLength: number = 0,
+	isGameFinished: boolean = false;
+
+
 enum COLORS {
 	SHEAD = "#FFF",
-	SBODY = "##E1E1E1",
+	SHEAD_SHADOW = "rgba(255,255,255,0.3 )",
+	SBODY = "#E1E1E1",
+	TILE_BORDER = "#222738",
+	TILE = "#181825",
+	APPLE = "red",
+	// TODO: find mild shadow color
+	APPLE_SHADOW = "red",
 }
 
 class Tile {
@@ -37,29 +52,126 @@ class Snake {
 	constructor() {
 		this.head = new Tile(2, 2, COLORS.SHEAD);
 		this.body = [];
-    }
-    
-    drawSnake = (): void => {
-        ctx.fillStyle = this.head.color;
-        ctx.shadowColor = "rgba(255,255,255,.3 )";
-        ctx.shadowBlur = 20;
-        ctx.fillRect(this.head.x * TILE_SIZE, this.head.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-        ctx.shadowBlur = 0;
-    }
+	}
+
+	changeSnakePositon = (): void => {
+		this.head.x += velocity[0];
+		this.head.y += velocity[1];
+	};
+
+	drawSnake = (): void => {
+		ctx.fillStyle = COLORS.SBODY;
+		for (let part of this.body) {
+			ctx.fillRect(part.x * TILE_SIZE, part.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+		}
+
+		this.body.push(this.head);
+		if (this.body.length > tailLength) {
+			this.body.shift();
+		}
+		
+		ctx.fillStyle = this.head.color;
+		ctx.shadowColor = COLORS.SHEAD_SHADOW;
+		ctx.shadowBlur = SHADOW_SIZE;
+		ctx.fillRect(
+			this.head.x * TILE_SIZE,
+			this.head.y * TILE_SIZE,
+			TILE_SIZE,
+			TILE_SIZE
+		);
+		ctx.shadowBlur = 0;
+	};
 }
+const snake: Snake = new Snake();
+
+class Apple {
+	position: Tile;
+	constructor() {
+		// TODO: create random colors for apple
+		
+		this.position = this.randomGenerator();
+	}
+
+	randomGenerator = (): Tile => {
+		const xPos: number = Math.floor(Math.random() * TILE_COUNT),
+			yPos: number = Math.floor(Math.random() * TILE_COUNT);
+		return new Tile(xPos, yPos, COLORS.APPLE);
+	};
+
+	drawApple = (): void => {
+		ctx.shadowColor = COLORS.APPLE_SHADOW;
+		ctx.shadowBlur = SHADOW_SIZE;
+		ctx.fillStyle = this.position.color;
+		ctx.fillRect(
+			this.position.x * TILE_SIZE,
+			this.position.y * TILE_SIZE,
+			TILE_SIZE,
+			TILE_SIZE
+		);
+		ctx.shadowBlur = 0;
+	};
+
+	checkAppleCollision = (): void => {
+		if (this.position.x == snake.head.x && this.position.y == snake.head.y) {
+			this.position = this.randomGenerator();
+			tailLength++;
+			score++;
+		}
+	};
+}
+const apple: Apple = new Apple();
 
 window.addEventListener("keydown", (e) => {
 	switch (e.code) {
 		case KEYS.DOWN:
+			if (velocity[2] == KEYS.UP) {
+				return;
+			}
+			velocity = [0, 1 , KEYS.DOWN];
+			break;
+		case KEYS.UP:
+			if (velocity[2] == KEYS.DOWN) {
+				return;
+			}
+			velocity = [0, -1, KEYS.UP];
+			break;
+		case KEYS.RIGHT:
+			if (velocity[2] == KEYS.LEFT) {
+				return;
+			}
+			velocity = [1, 0 , KEYS.RIGHT];
+			break;
+		case KEYS.LEFT:
+			if (velocity[2] == KEYS.RIGHT) {
+				return;
+			}
+			velocity = [-1, 0 , KEYS.LEFT];
 			break;
 	}
 });
-const snake: Snake = new Snake();
 
 class Game {
-    drawGame = (deltaTime: number): void => {
-        
-        snake.drawSnake();
+	drawTileMap = (): void => {
+		ctx.strokeStyle = COLORS.TILE_BORDER;
+		ctx.fillStyle = COLORS.TILE;
+		for (let i: number = 0; i < TILE_COUNT; i++) {
+			for (let j: number = 0; j < TILE_COUNT; j++) {
+				ctx.fillRect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+				ctx.strokeRect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+			}
+		}
+	};
+
+	drawGame = (time: number): void => {
+		if (time - deltaTime < speed) {
+		} else {
+			snake.changeSnakePositon();
+			apple.checkAppleCollision();
+			this.drawTileMap();
+			apple.drawApple();
+			snake.drawSnake();
+			deltaTime = time;
+		}
 		requestAnimationFrame(this.drawGame);
 	};
 }
